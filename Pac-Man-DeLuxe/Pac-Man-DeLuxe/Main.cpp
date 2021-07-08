feab17408057198f88ce8b2dddf33d431f86cb4b
Alexander.Apostolov <alexander.apostolov@softwaregroup-bg.com>
2021-07-08 03:08:13 +0300

    chore: Added style guide link

--- Pac-Man-DeLuxe/Pac-Man-DeLuxe/Main.cpp
+++ Pac-Man-DeLuxe/Pac-Man-DeLuxe/Main.cpp
@@ -2,13 +2,15 @@
 #include "SDL.h"
 #include "SDL_main.h"
 
-/*
-On Windows SDL provides WinMain(), which parses the command lineand passes
-the arguments to your main function.
+// Following the Google C++ Style Guide 
+// https://google.github.io/styleguide/cppguide.html
 
-SDL #defines main as SDL_main and calls it internally.
-The application's SDL_main is required to have C linkage.
-*/
+// SDL main:
+// On Windows SDL provides WinMain(), which parses the command lineand passes
+// the arguments to your main function.
+// 
+// SDL #defines main as SDL_main and calls it internally.
+// The application's SDL_main is required to have C linkage.
 
 #ifdef __cplusplus
 extern "C"
