2179823ca8df6b59d522ec7bc292f95b79c93275
Alexander.Apostolov <alexander.apostolov@softwaregroup-bg.com>
2021-07-12 23:29:24 +0300

    feat: Reworked Map and Tiles; Added AssetLoader
    Reworked Map:
    - now uses a double array instead of double vector
    - added destructor to free the memory for the tiles
    - changed symbols that map to tile types
    Reworked Tile:
    - narrowed tile types to nine
    - added tiles spritesheet, texture is stored in AssetLoader
    - added boolean fields contains_player_ and contains_ghost_
    Added AssetLoader:
    - initializes SDL_image
    - loads predefined assets - currently the spritesheets
    - changed render methods to void Render(renderer, asset_loader)
    - AssetLoader is passed to the render methods by the Game class
    - unloads SDL_image libraries in destructor
    Rendered first decent looking map! 🥳

---
+++ Pac-Man-DeLuxe/Pac-Man-DeLuxe/AssetLoader.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "AssetLoader.h"
+
+AssetLoader::AssetLoader() {
+	this->units_spritesheet_ = nullptr;
+	this->tiles_spritesheet_ = nullptr;
+}
+
+AssetLoader::~AssetLoader() {
+	// Unload the dynamically loaded image libraries
+	IMG_Quit();
+}
+
+int AssetLoader::Init() {
+	int format_flags = IMG_INIT_PNG;
+	// Load the image libraries for the given formats
+	int img_init = IMG_Init(format_flags);
+	if ((img_init & format_flags) != format_flags) {
+		std::cout << "IMG init error: " << SDL_GetError() << std::endl;
+		return -1;
+	}
+
+	std::cout << "Asset loader initialized." << std::endl;
+	return 0;
+}
+
+int AssetLoader::LoadAssets(SDL_Renderer* renderer) {
+	// Load the units spritesheet
+	// From Image to Surface to Texture
+	SDL_Surface* tempSurface = IMG_Load("Assets/Spritesheets/pac-man-ghosts-sprites.png");
+	if (tempSurface == nullptr) {
+		std::cout << "IMG load error: " << SDL_GetError() << std::endl;
+		return -1;
+	}
+	this->units_spritesheet_ = SDL_CreateTextureFromSurface(renderer, tempSurface);
+
+	// Load the units spritesheet
+	// Reuse tempSurface
+	tempSurface = IMG_Load("Assets/Spritesheets/pac-man-tiles-sprites.png");
+	if (tempSurface == nullptr) {
+		std::cout << "IMG load error: " << SDL_GetError() << std::endl;
+		return -1;
+	}
+	this->tiles_spritesheet_ = SDL_CreateTextureFromSurface(renderer, tempSurface);
+
+	// Free the surface
+	SDL_FreeSurface(tempSurface);
+
+	std::cout << "Assets loaded." << std::endl;
+	return 0;
+}
--- Pac-Man-DeLuxe/Pac-Man-DeLuxe/Map.cpp
+++ Pac-Man-DeLuxe/Pac-Man-DeLuxe/Map.cpp
@@ -1,14 +1,20 @@
-﻿#include "Map.h"
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
+#include "Map.h"
 
-Map::Map(int x, int y) {
-	this->tile_matrix_ = std::vector<std::vector<Tile*>>(x, std::vector<Tile*>(y, nullptr));
-	this->size_x_ = x;
-	this->size_y_ = y;
+Map::Map() {
+	this->file_name_ = nullptr;
 }
 
-Map::~Map() {}
+Map::~Map() {
+	for (int i = 0; i < MAP_WIDTH_IN_TILES; i++)
+	{
+		for (int j = 0; j < MAP_HEIGHT_IN_TILES; j++)
+		{
+			delete this->tile_matrix_[i][j];
+		}
+	}
+}
 
 void Map::LoadMapFromFile(const char* file_name) {
 	std::ifstream ifs;
@@ -16,84 +22,66 @@ void Map::LoadMapFromFile(const char* file_name) {
 	// TODO: Prefix file name with Assets/
 	ifs.open(file_name, std::ifstream::in);
 
+	std::cout << "Map file opened." << std::endl;
+
 	if (!ifs.is_open()) {
 		std::cout << "Error opening file." << std::endl;
 	}
 
 	char c;
+	// Columns = width
 	int tile_x = 0;
+	// Rows = height
 	int tile_y = 0;
-	Tile* new_tile;
 	while (ifs.get(c)) {
 		switch (c)
 		{
 		case '.':
-			// Set proper tile
-			new_tile = new Tile(
-				tile_x,
-				tile_y,
-				this->TILE_WIDTH_,
-				this->TILE_HEIGHT_,
-				EMPTY
-			);
-			this->SetTile(tile_x, tile_y, new_tile);
-			tile_y++;
+			// Set proper tile and increase the column counter
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, EMPTY));
+			tile_x++;
 			break;
 		case '=':
-			// Set proper tile
-			new_tile = new Tile(
-				tile_x,
-				tile_y,
-				this->TILE_WIDTH_,
-				this->TILE_HEIGHT_,
-				WALL_HORIZONTAL_TOP
-			);
-			this->SetTile(tile_x, tile_y, new_tile);
-			tile_y++;
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, WALL_HORIZONTAL_TOP));
+			tile_x++;
 			break;
-		case '═':
-			// Set proper tile
-			new_tile = new Tile(
-				tile_x,
-				tile_y,
-				this->TILE_WIDTH_,
-				this->TILE_HEIGHT_,
-				WALL_HORIZONTAL_TOP
-			);
-			this->SetTile(tile_x, tile_y, new_tile);
-			tile_y++;
+		case '-': // \u2550 = 45 somehow
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, WALL_HORIZONTAL_BOTTOM));
+			tile_x++;
 			break;
 		case '|':
-			// Set proper tile
-			new_tile = new Tile(
-				tile_x,
-				tile_y,
-				this->TILE_WIDTH_,
-				this->TILE_HEIGHT_,
-				WALL_VERTICAL_LEFT
-			);
-			this->SetTile(tile_x, tile_y, new_tile);
-			tile_y++;
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, WALL_VERTICAL_LEFT));
+			tile_x++;
 			break;
 		case '!':
-			// Set proper tile
-			new_tile = new Tile(
-				tile_x,
-				tile_y,
-				this->TILE_WIDTH_,
-				this->TILE_HEIGHT_,
-				WALL_VERTICAL_RIGHT
-			);
-			this->SetTile(tile_x, tile_y, new_tile);
-			tile_y++;
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, WALL_VERTICAL_RIGHT));
+			tile_x++;
+			break;
+		case ']':
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, WALL_ROUNDED_TOP_LEFT));
+			tile_x++;
+			break;
+		case '[':
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, WALL_ROUNDED_TOP_RIGHT));
+			tile_x++;
+			break;
+		case ')':
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, WALL_ROUNDED_BOTTOM_LEFT));
+			tile_x++;
+			break;
+		case '(': // \u2554 = 43 somehow
+			this->SetTile(tile_x, tile_y, new Tile(tile_x, tile_y, WALL_ROUNDED_BOTTOM_RIGHT));
+			tile_x++;
 			break;
 		case '\n':
-			tile_x++;
-			tile_y = 0;
+			// When we reach a newline set the column to zero and increase the row counter
+			tile_x = 0;
+			tile_y++;
 			break;
 		}
 	}
 
+	std::cout << "Map loaded." << std::endl;
 }
 
 Tile* Map::GetTile(int x, int y) {
@@ -104,13 +92,17 @@ void Map::SetTile(int x, int y, Tile* tile) {
 	this->tile_matrix_[x][y] = tile;
 }
 
-void Map::Render(SDL_Renderer* renderer) {
-	for (size_t i = 0; i < this->size_x_; i++)
+void Map::Render(SDL_Renderer* renderer, AssetLoader* asset_loader) {
+	// The built-in array uses signed subscripts.
+	// Given the known problems with unsigned and signed/unsigned mixtures, better stick to (signed) integers of a sufficient size.
+	// https://github.com/isocpp/CppCoreGuidelines/blob/master/CppCoreGuidelines.md#es107-dont-use-unsigned-for-subscripts-prefer-gslindex
+
+	for (int i = 0; i < MAP_WIDTH_IN_TILES; i++)
 	{
-		for (size_t j = 0; j < this->size_y_; j++)
+		for (int j = 0; j < MAP_HEIGHT_IN_TILES; j++)
 		{
 			// Render tile
-			this->tile_matrix_[i][j]->render(renderer);
+			this->tile_matrix_[i][j]->render(renderer, asset_loader);
 		}
 	}
 }
